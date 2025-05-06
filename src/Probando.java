0f7e046237ba0963deea86dabf6ba0615ead5769
kluhroth <aitorfuenla1231@gmail.com>
2025-05-06 10:39:32 +0200

    Update Probando.java

--- src/Probando.java
+++ src/Probando.java
@@ -1,5 +1,7 @@
 public class Probando {
     public static void main(String[] args) {
         System.out.println("probando Github");
+        System.out.println("editando Github desde github");
+        
     }
 }
