97f6b66dc26b56038bbca14534ae70f0c23b34e6
kluhroth <aitorfuenla1231@gmail.com>
2025-05-06 19:14:54 +0200

    Update Probando.java

--- src/Probando.java
+++ src/Probando.java
@@ -3,6 +3,8 @@ package PracticafinalEntornos;
 public class PracticaFinal {
     public static void main(String[] args) {
         System.out.println("practicaFinal de Github");
+        System.out.println("Modificacion para ver si se ve en local");
+        
 
 
     }
