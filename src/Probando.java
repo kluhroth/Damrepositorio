abf5c3fc60e5d8a044f0ba51b5e1e1f9beb48b9a
kluhroth <aitorfuenla1231@gmail.com>
2025-05-06 19:13:28 +0200

    Update Probando.java

--- src/Probando.java
+++ src/Probando.java
@@ -1,7 +1,10 @@
+package PracticafinalEntornos;
+
 public class PracticaFinal {
     public static void main(String[] args) {
         System.out.println("practicaFinal de Github");
-        
-        
+
+
     }
 }
+
